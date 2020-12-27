dd55d2e36e508122245af203ce5f54fe31534f47
omertb <omertugrul@gmail.com>
2020-12-27 23:18:58 +0300

    sample request and get json result

--- project/apicalls.py
+++ project/apicalls.py
@@ -6,4 +6,4 @@ def get_bgplay(ip_or_asn):
     start_time = int(time()) - 3600  # since before one hour
     url = f"https://stat.ripe.net/data/bgplay/data.json?&unix_timestamps=TRUE&starttime={start_time}&resource={ip_or_asn}"
     response = requests.request("GET", url)
-    return response
+    return response.text
--- project/main/views.py
+++ project/main/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, render_template, flash
 from project.main.forms import MainForm
+from project.apicalls import *
+from pprint import pprint
 
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
@@ -12,9 +14,9 @@ def main():
     if request.method == 'POST':
         if form.validate_on_submit():
             ip_or_asn = form.ip_or_asn.data
-            print(ip_or_asn)
-            print(request.form['ip_or_asn'])
-            flash("Entry is {}".format(ip_or_asn))
+            result = get_bgplay(ip_or_asn)
+            pprint(result)
+            flash("Result is \n{}".format(result))
         else:
             print("NOT VALID!")
     return render_template('main.html', form=form, error=error)
